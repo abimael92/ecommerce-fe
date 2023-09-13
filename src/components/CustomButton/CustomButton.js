ca38c46c01bef89b1d7f408e57706f7462428e84
Abimael Garcia <Abimael.Garcia@improving.com>
2023-09-13 17:12:48 -0600

    instal sass, implement and define variables

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+export const CustomButton = () => {
+	return (
+		<div>
+			<Button primary>Primary</Button>
+			<Button secondary>Secondary</Button>
+		</div>
+	);
+};
