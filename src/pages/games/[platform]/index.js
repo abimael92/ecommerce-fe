bec4516c770360429cff24c3d45d66af771fea9e
Abimael Garcia <Abimael.Garcia@improving.com>
2023-11-28 15:28:55 -0600

    first attempt

--- src/pages/games/[platform]/index.js
+++ src/pages/games/[platform]/index.js
@@ -3,31 +3,40 @@ import { Platform, Game } from '@/api'
 export { default } from './platform'
 
 export async function getServerSideProps(context) {
-  const {
-    params: { platform },
-    query: { page = 1 },
-  } = context
+  const { query, params } = context
+
+  const { page = 1 } = query
+  const { platform } = params
+  // const {
+  //   params: { platform },
+  //   query: { page = 1 },
+  // } = context
+
+  console.log(`\ngetServerSideProps Platform :${platform}\n`)
 
   const platformCtrl = new Platform()
+  let responsePlatform = null
 
   try {
-    const responsePlatform = await platformCtrl.getBySlug(platform)
+    console.log(`\ before platform response in getbyslug\n\n`)
+    responsePlatform = await platformCtrl.getBySlug(platform)
     console.log(`\n\nPlatform data:${responsePlatform}\n`)
   } catch (error) {
     console.log(`\n\ Error while fetching platform data: ${error}\n`)
-    // Handle the error or log it appropriately
   }
 
-  // const responsePlatform = await platformCtrl.getBySlug(platform);
+  // const responsePlatform = await platformCtrl.getBySlug(platform);⁄
 
   const gameCtrl = new Game()
-  const responseGames = await gameCtrl.getGamesByPlatformSlug(platform, page)
+  // const responseGames = await gameCtrl.getGamesByPlatformSlug(platform, page)
 
   return {
     props: {
       platform: responsePlatform,
-      games: responseGames.data,
-      pagination: responseGames.meta.pagination,
+      // games: responseGames.data,
+      // pagination: responseGames.meta.pagination,
+      games: null,
+      pagination: null,
     },
   }
 }
